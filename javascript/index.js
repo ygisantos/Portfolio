cd2daf59feda3e47d40cc36ef3144a0db4993856
DESKTOP-RCS1AH7\Xeb <ygigamer@gmail.com>
2024-11-20 22:37:02 +0800

    Finished awards and certificates

--- javascript/index.js
+++ javascript/index.js
@@ -18,6 +18,26 @@ const awardDictionary = {
     5: {
         name: "SAP BUSINESS ONE",
         imageURL: "../images/certificates/SAP.jpg"
+    },
+    2.1: {
+        name: "JAME GAM CHRISTMAS EDITION",
+        imageURL: "../images/Awards/JAM_GAME.PNG",
+        description: "A 4TH Place Screenshot as a Solo Team for Online Game Jam Competition with over 90 Team Participants"
+    },
+    2.2: {
+        name: "SCORESPACE JAM #17",
+        imageURL: "../images/Awards/SCORE_SPACE17.PNG",
+        description: "A 10TH Place Screenshot as a Solo Team for Online Game Jam Competition with over 140 Team Participants"
+    },
+    2.3: {
+        name: "CODEFEST (MOBILE APP HACKATHON)",
+        imageURL: "../images/Awards/HACKATHON.jpg",
+        description: "A First Runner-up Certification for Local Competition held at STI College Balagtas"
+    },
+    2.4: {
+        name: "NATIONAL IT SKILLS COMPETITION (JAVA)",
+        imageURL: "../images/Awards/NATIONAL.jpg",
+        description: "A Participation Certificate for National Competition of Java Algorithm"
     }
 }
 
@@ -31,6 +51,13 @@ document.querySelectorAll('.award-btn').forEach(button => {
         const data = awardDictionary[button.value];
         document.getElementById('award-title').innerText = data.name;
         document.getElementById('award-img').src = data.imageURL;
+
+        const info = document.getElementById('award-info');
+        if(!data.description) info.style.display = 'none';
+        else {
+            info.style.display = 'block';
+            info.innerText = data.description;
+        }
     });
 });
 
